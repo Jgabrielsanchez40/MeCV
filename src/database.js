285efe621b74ba6b2ed77385de22e703360e005c
Jgabrielsanchez40 <jgabrielsanchez40@gmail.com>
2021-07-11 17:28:01 -0400

    dos

--- src/database.js
+++ src/database.js
@@ -3,7 +3,6 @@ const config = require('./config.js');
 
 
 const uri = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.1mzxk.mongodb.net/${config.DBNAME}?retryWrites=true`;
-//mongoose.connect('mongodb://localhost/notes-db-app', {
 mongoose.connect(uri, {
   useCreateIndex: true,
   useNewUrlParser: true,
@@ -11,5 +10,3 @@ mongoose.connect(uri, {
   useFindAndModify: false
 
 });
-     // .then(db => console.log('DB is connected'))
-     // .catch(err => console.error(err));   mongoose.connect('mongodb+srv://josegs:J@se101202@cluster0.1mzxk.mongodb.net/MyRCV?retryWrites=true', {
